9f5f19a65eedf1bc9be82ec3f3b42c92142b2b8e
alok87 <alok.singh@practo.com>
2017-07-10 21:34:35 +0530

    Moved Seed to Init()

--- pkg/random/random.go
+++ pkg/random/random.go
@@ -5,6 +5,10 @@ import (
     "time"
 )
 
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
 // Return: A random range of numbers as a slice within a range
 //
 // Usage: 
@@ -17,7 +21,6 @@ func RangeInt(min int, max int, n int) []int {
      arr := make([]int, n)
      var r int
      for r = 0; r <= n-1; r++ {
-         rand.Seed(time.Now().UnixNano())
          arr[r] = rand.Intn(max) + min
      }
      return arr
